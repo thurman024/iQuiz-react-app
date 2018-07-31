fe1e234082f24df3c4e39bd28f61b7ccf6450477
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-07-30 21:01:57 -0500

    add scores controller

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,17 @@
+class ScoresController < ApplicationController
+  def index
+    @scores = Scores.top_ten
+    render json: @scores
+  end
+
+  def update
+    @score = Score.find_by(id: params[:id])
+    @score.update(score_params)
+  end
+
+  private
+
+  def score_params
+    params.permit(:right_answers, :total_questions)
+  end
+end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,7 @@
 class Score < ApplicationRecord
   belongs_to :user
+
+  def self.top_ten
+    order("right_answers").limit(10)
+  end
 end
